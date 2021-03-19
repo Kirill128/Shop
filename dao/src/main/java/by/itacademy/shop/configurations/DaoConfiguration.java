842d53839cba153b4e42760a171edbd9feaf97c2
Kirill <liw.liw510@gmail.com>
2021-03-19 16:02:45 +0300

    s

--- dao/src/main/java/by/itacademy/shop/configurations/DaoConfiguration.java
+++ dao/src/main/java/by/itacademy/shop/configurations/DaoConfiguration.java
@@ -7,9 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan(basePackages = "by.itacademy.shop.dao")
 public class DaoConfiguration {
+    @Bean
+    public DataSource dataSource(){
+
+    }
+
     @Bean
     public SpringLiquibase liquibase() {
         SpringLiquibase liquibase = new SpringLiquibase();
